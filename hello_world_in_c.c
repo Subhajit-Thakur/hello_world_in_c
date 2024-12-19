31b26b661b809e4c4573762ae1abf72b7ef5c335
Subhajit Thakur <sthakurcs16@gmail.com>
2024-12-19 17:45:11 +0530

    hello world in c

---
+++ hello_world_in_c.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to this C programming tutorial!
+  Learn C programming step by step with simple examples. Perfect for beginners. 
+  Created by Subhajit Thakur.
+
+*******************************************************************************/
+
+/* code starts here */
+
+#include<stdio.h>
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
+
+
+/* code ends here */
