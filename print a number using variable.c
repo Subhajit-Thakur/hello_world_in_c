70b6f9f19db2929f7af6d89e45da35d69782f33e
Subhajit Thakur <sthakurcs16@gmail.com>
2024-12-31 00:32:52 +0530

    printing a number

---
+++ print a number using variable.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to this C programming tutorial!
+  Learn C programming step by step with simple examples. Perfect for beginners. 
+  Created by Subhajit Thakur.
+
+*******************************************************************************/
+
+/* code starts here */
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a number you want to print : ");
+    scanf("%d",&n);
+    printf("The number is : %d",n);
+    return 0;
+}
+
+/* code ends here */
